ce96ce6a099452f6a7d51d084b8f33578a1020a4
Leechaelin <elisa0511@naver.com>
2017-03-28 10:23:33 +0900

    modify 1

--- app/src/main/java/com/example/leechaelin/hw_3/MainActivity.java
+++ app/src/main/java/com/example/leechaelin/hw_3/MainActivity.java
@@ -8,85 +8,114 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView t1;
+    TextView t1,t2;
     ImageView i,i2;
-    LinearLayout l;
+    LinearLayout l,l2;
+    FrameLayout f;
     int degree = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        l = (LinearLayout)findViewById(R.id.linear);
+        l2 = (LinearLayout)findViewById(R.id.linear2);
+        f = (FrameLayout)findViewById(R.id.frame);
         t1 = (TextView)findViewById(R.id.text);
+        t2 = (TextView)findViewById(R.id.text2);
         i = (ImageView)findViewById(R.id.image);
         i2 = (ImageView)findViewById(R.id.image2);
-        l = (LinearLayout)findViewById(R.id.activity_main);
-
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
         return super.onCreateOptionsMenu(menu);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
                 case R.id.red:
-                    l.setBackgroundColor(Color.RED);
+                    f.setBackgroundColor(Color.RED);
                     break;
                 case R.id.blue:
-                    l.setBackgroundColor(Color.BLUE);
+                    f.setBackgroundColor(Color.BLUE);
                     break;
                 case R.id.yellow:
-                    l.setBackgroundColor(Color.YELLOW);
+                    f.setBackgroundColor(Color.YELLOW);
                     break;
+
                 case R.id.chicken:
+                    l2.setVisibility(View.INVISIBLE);
                     i.setImageResource(R.drawable.chicken);
-                    i2.setVisibility(View.INVISIBLE);
+                    l.setVisibility(View.VISIBLE);
                     item.setChecked(true);
                     break;
                 case R.id.spagetti:
-                    i.setVisibility(View.INVISIBLE);
+                    l.setVisibility(View.INVISIBLE);
                     i2.setImageResource(R.drawable.spa);
+                    l2.setVisibility(View.VISIBLE);
                     item.setChecked(true);
                     break;
+
                 case R.id.showtitle:
                     if(item.isChecked()){
                         t1.setVisibility(View.INVISIBLE);
+                        t2.setVisibility(View.INVISIBLE);
                         item.setChecked(false);
-                    } else if(item.isChecked()==false){
-                        t1.setVisibility(View.VISIBLE);
-                        if(i.getVisibility()==View.INVISIBLE){
-                            t1.setText(String.format(String.valueOf("새콤한 스파게티 ")));
-                        }else if(i2.getVisibility()==View.INVISIBLE){
-                            t1.setText(String.format(String.valueOf("맛있는 치")));
+                    }else if(item.isChecked()==false){
+                        if(l.getVisibility()==View.VISIBLE){
+                            t1.setText(String.format(String.valueOf("맛있는 치킨 ")));
+                            t1.setVisibility(View.VISIBLE);
+                            item.setChecked(true);
+                        }else if(l2.getVisibility()==View.VISIBLE){
+                            t2.setText(String.format(String.valueOf("상큼한 스파게티 ")));
+                            t2.setVisibility(View.VISIBLE);
                         }
                         item.setChecked(true);
                     }
+
                     break;
                 case R.id.zoom:
                     if(item.isChecked()){
-                        i.setScaleX(1);
-                        i.setScaleY(1);
+                        if(l.getVisibility()==View.VISIBLE){
+                            i.setScaleX(1);
+                            i.setScaleY(1);
+                        }else if(l2.getVisibility()==View.VISIBLE){
+                            i2.setScaleX(1);
+                            i2.setScaleY(1);
+                        }
                         item.setChecked(false);
-                        break;
+
                     }
                     else if(item.isChecked()==false){
-                        i.setScaleX(2);
-                        i.setScaleY(2);
+                        if(l.getVisibility()==View.VISIBLE){
+                            i.setScaleX(2);
+                            i.setScaleY(2);
+                        }else if(l2.getVisibility()==View.VISIBLE){
+                            i2.setScaleX(2);
+                            i2.setScaleY(2);
+                        }
                         item.setChecked(true);
-                        break;
+
                     }
+                    break;
 
                  case R.id.move:
-                     i.setRotation(degree+30);
+                     if(l.getVisibility()==View.VISIBLE){
+                         i.setRotation(degree+30);
+                     }
+                     else if(l2.getVisibility()==View.VISIBLE){
+                         i2.setRotation(degree+30);
+                     }
                      break;
 
         }
